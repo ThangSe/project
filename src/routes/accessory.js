b096293b082b40b9d8ce3e18fbe177855c417d1e
thang <thangvhv99@gmail.com>
2022-08-08 20:02:10 +0700

    update07

--- src/routes/accessory.js
+++ src/routes/accessory.js
@@ -2,7 +2,7 @@ const accessoriesController = require("../app/controllers/AccessoriesController"
 const middlewareController = require("../app/controllers/MiddlewareController")
 const router = require("express").Router()
 
-router.get("/all-accessories", middlewareController.verifyTokenStaff, accessoriesController.showAllAccessory)
+router.get("/all-accessories", middlewareController.verifyToken, accessoriesController.showAllAccessory)
 router.post("/create-accessory", middlewareController.verifyTokenManager, accessoriesController.createNewAccessory)
 router.post("/create-supplier", middlewareController.verifyTokenManager, accessoriesController.createSupplier)
 module.exports = router
