66246f6c0efd5bc1457ab8e98336c1b426edf44e
thang <thangvhv99@gmail.com>
2022-08-18 05:39:36 +0700

    update07

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -96,6 +96,7 @@ class OrderController {
                     }
                 }
             ])
+            res.status(200).json(order)
         } catch (err) {
             res.status(500).json(err)
         }
