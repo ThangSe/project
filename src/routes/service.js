f8bb2d667e9b7740ab2f13f5ad4db6f93824eb2c
thang <thangvhv99@gmail.com>
2022-08-19 01:45:34 +0700

    update07

--- src/routes/service.js
+++ src/routes/service.js
@@ -3,7 +3,7 @@ const middlewareController = require("../app/controllers/MiddlewareController")
 const router = require("express").Router()
 
 router.get("/all-service", middlewareController.verifyToken, serviceController.showAllService)
-router.get("all-accessories-service/:serviceId", serviceController.showAllAccessoriesOfSerive)
+router.get("/all-accessories-service/:serviceId", serviceController.showAllAccessoriesOfSerive)
 router.post("/new-service", middlewareController.verifyTokenManager, serviceController.createNewService)
 router.get("/accessories-to-service/:serviceId",middlewareController.verifyTokenManager ,serviceController.addMoreAccessoriesToService)
 router.get("/:serviceId", middlewareController.verifyTokenStaff, serviceController.getService)
