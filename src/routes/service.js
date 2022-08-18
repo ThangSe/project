aab4fc6c4b17ff091d4b4f361b06e955f584eec2
thang <thangvhv99@gmail.com>
2022-08-19 01:43:25 +0700

    update07

--- src/routes/service.js
+++ src/routes/service.js
@@ -6,6 +6,6 @@ router.get("/all-service", middlewareController.verifyToken, serviceController.s
 router.get("all-accessories-service/:serviceId", serviceController.showAllAccessoriesOfSerive)
 router.post("/new-service", middlewareController.verifyTokenManager, serviceController.createNewService)
 router.get("/accessories-to-service/:serviceId",middlewareController.verifyTokenManager ,serviceController.addMoreAccessoriesToService)
-router.get("/:id", middlewareController.verifyTokenStaff, serviceController.getService)
+router.get("/:serviceId", middlewareController.verifyTokenStaff, serviceController.getService)
 router.patch("/:id", middlewareController.verifyTokenManager, serviceController.updateService)
 module.exports = router
