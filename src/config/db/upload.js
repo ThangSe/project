aeb66f966d889689aa9d9a107949a658d6b7af59
thang <thangvhv99@gmail.com>
2022-08-25 15:26:52 +0700

    update07

--- src/config/db/upload.js
+++ src/config/db/upload.js
@@ -1,4 +1,15 @@
 const {GridFsStorage} = require('multer-gridfs-storage')
+const mongoose = require('mongoose')
+const Grid = require('gridfs-stream')
+const conn = mongoose.createConnection(process.env.DB_CONNECTION)
+let gfs, gridfsBucket
+conn.once('open', () => {
+    gridfsBucket = new mongoose.mongo.GridFSBucket(conn.db, {
+        bucketName: 'uploads'
+      })
+    gfs = Grid(conn.db, mongoose.mongo)
+    gfs.collection('uploads')
+})
 const storage = new GridFsStorage({
     url: process.env.DB_CONNECTION,
     file: (req, file) => {
